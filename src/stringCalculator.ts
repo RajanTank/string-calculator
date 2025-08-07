c4b40248902e5b033ad6a058fe4ff28b71ffde23
RajanTank <rajantank731@gmail.com>
2025-08-07 22:23:47 +0530

    (Feat) Updated add method to handle custom single-char delimiter

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,8 +2,12 @@ export class StringCalculator {
   public Add(numbers: string): number {
     if (!numbers) return 0;
     const delimiters = [",", "\n"];
-    const regex = new RegExp(delimiters.join("|"));
 
+    if (numbers.startsWith("//")) {
+      delimiters.push(numbers[2]);
+      numbers = numbers.slice(numbers.indexOf("\n") + 1);
+    }
+    const regex = new RegExp(delimiters.join("|"));
     const numberArray = numbers.split(regex).map(n => parseInt(n));
     return numberArray.reduce((sum, number) => sum + number, 0);
   }
