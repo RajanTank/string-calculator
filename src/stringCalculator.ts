6778e771d0358b404b465d1c7a3efe71835e39e1
RajanTank <rajantank731@gmail.com>
2025-08-07 21:56:23 +0530

    (Feat) Updated add method to handle new line between numbers

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,7 +1,10 @@
 export class StringCalculator {
   public Add(numbers: string): number {
     if (!numbers) return 0;
-    const numberArray = numbers.split(",").map(n => parseInt(n));
+    const delimiters = [",", "\n"];
+    const regex = new RegExp(delimiters.join("|"));
+
+    const numberArray = numbers.split(regex).map(n => parseInt(n));
     return numberArray.reduce((sum, number) => sum + number, 0);
   }
 }
