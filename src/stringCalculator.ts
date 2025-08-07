553d0cc3c5a224e72ac2db53a409062a626fc245
RajanTank <rajantank731@gmail.com>
2025-08-07 22:56:28 +0530

    (Refactor) Code refactor for delimiter parsing

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,14 +1,10 @@
 export class StringCalculator {
   public Add(numbers: string): number {
     if (!numbers) return 0;
-    const delimiters = [",", "\n"];
 
-    if (numbers.startsWith("//")) {
-      delimiters.push(numbers[2]);
-      numbers = numbers.slice(numbers.indexOf("\n") + 1);
-    }
-    const regex = new RegExp(delimiters.join("|"));
-    const numberArray = numbers.split(regex).map(n => parseInt(n));
+    const { delimiters, sanitizedNumbers } = this.getDelimiters(numbers);
+
+    const numberArray = this.parsedNumbers(sanitizedNumbers, delimiters);
 
     this.checkForNegative(numberArray);
 
@@ -20,6 +16,25 @@ export class StringCalculator {
     if (negatives.length > 0) {
       throw new Error(`negatives not allowed: ${negatives.join(", ")}`);
     }
+  }
+  parsedNumbers(numbers: string, delimiters: string[]): number[] {
+    const regex = new RegExp(delimiters.join("|"));
+    return numbers.split(regex).map(n => parseInt(n));
+  }
+  getDelimiters(numbers: string): { delimiters: string[]; sanitizedNumbers: string } {
+    const delimiters = [",", "\n"];
+    if (numbers.startsWith("//")) {
+      delimiters.push(numbers[2]);
+      const sanitizedNumbers = numbers.slice(numbers.indexOf("\n") + 1); // To ge the numbers after the delimiter e.g //;\n1;2 will be 1;2
+      return {
+        delimiters,
+        sanitizedNumbers
+      }
+    }
+    return {
+      delimiters,
+      sanitizedNumbers: numbers
+    };
   }
 }
 
