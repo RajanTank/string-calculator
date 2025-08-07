4a158eb8432f805d7cdd2f178d0dfce5aa8572be
RajanTank <rajantank731@gmail.com>
2025-08-07 23:20:46 +0530

    (Feat) Update code for support multiple & multi character custom delimiters

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -3,11 +3,8 @@ export class StringCalculator {
     if (!numbers) return 0;
 
     const { delimiters, sanitizedNumbers } = this.getDelimiters(numbers);
-
     const numberArray = this.parsedNumbers(sanitizedNumbers, delimiters);
-
     this.checkForNegative(numberArray);
-
     return numberArray.filter(num => num <= 1000).reduce((sum, number) => sum + number, 0);
   }
 
@@ -18,23 +15,39 @@ export class StringCalculator {
     }
   }
   parsedNumbers(numbers: string, delimiters: string[]): number[] {
-    const regex = new RegExp(delimiters.join("|"));
+    const regex = new RegExp(delimiters.map(m => this.escapeRegex(m)).join("|"));
     return numbers.split(regex).map(n => parseInt(n));
   }
   getDelimiters(numbers: string): { delimiters: string[]; sanitizedNumbers: string } {
-    const delimiters = [",", "\n"];
-    if (numbers.startsWith("//")) {
-      delimiters.push(numbers[2]);
-      const sanitizedNumbers = numbers.slice(numbers.indexOf("\n") + 1); // To ge the numbers after the delimiter e.g //;\n1;2 will be 1;2
+    const defaultDelimiters = [",", "\n"];
+
+    if (!numbers.startsWith("//")) {
       return {
-        delimiters,
-        sanitizedNumbers
-      }
+        delimiters: defaultDelimiters,
+        sanitizedNumbers: numbers
+      };
+    }
+
+    const delimiterSectionEnd = numbers.indexOf("\n");
+    const delimiterSection = numbers.slice(2, delimiterSectionEnd);  // To retrieve delimiter portion only eg. [***]
+    const sanitizedNumbers = numbers.slice(delimiterSectionEnd + 1);  // To ge the numbers after the delimiter e.g //;\n1;2 will be 1;2
+    let customDelimiters: string[];
+
+    if (delimiterSection.startsWith("[")) {
+      const delimiterMatches = [...delimiterSection.matchAll(/\[([^\]]+)\]/g)]; // To get the match delimiters eg. [***]
+      customDelimiters = delimiterMatches.map(m => m[1]);
+    } else {
+      customDelimiters = [delimiterSection];
     }
     return {
-      delimiters,
-      sanitizedNumbers: numbers
+      delimiters: [...defaultDelimiters, ...customDelimiters],
+      sanitizedNumbers
     };
+  }
+  // https://stackoverflow.com/a/6969486
+  // To escape regex special characters
+  escapeRegex(str: string): string {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
   }
 }
 
